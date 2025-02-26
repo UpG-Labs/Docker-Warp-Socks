b2c68b03701c2dad766b97d0b5a2bab375e50562
romracer <romracer@users.noreply.github.com>
2025-02-26 05:10:49 +0000

    Add some CIDR ranges back in

--- dev/v4/entrypoint.sh
+++ dev/v4/entrypoint.sh
@@ -103,6 +103,16 @@ cat <<EOF | tee /etc/sing-box/config.json
             {
                 "ip_is_private": true,
                 "outbound": "direct-out"
+            },
+            {
+                "ip_cidr": [
+                    "10.0.0.0/8",
+                    "127.0.0.0/8",
+                    "169.254.0.0/16",
+                    "172.16.0.0/12",
+                    "192.168.0.0/16"
+                ],
+                "outbound": "direct-out"
             }
         ],
         "auto_detect_interface": true,
